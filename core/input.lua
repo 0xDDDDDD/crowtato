d50933e69343dba7d6dbe9ce97fca8568476118d
Damon <Damon.Connolly@proton.me>
2025-09-25 06:58:06 +0100

    Updated input module to handle discreet input

--- core/input.lua
+++ core/input.lua
@@ -8,7 +8,8 @@ function Input:new(context)
 
     input.actions = {
         moveX = 0,
-        moveY = 0
+        moveY = 0,
+        pressed = {}
     }
 
     input.mouse = {
@@ -17,21 +18,45 @@ function Input:new(context)
         lmb = false
     }
 
+    input.prev = {}
+
     return input
 end
 
 function Input:update()
 
+    --movement input
     self.actions.moveX  = (love.keyboard.isDown("d") and 1 or 0)
                         - (love.keyboard.isDown("a") and 1 or 0)
     self.actions.moveY  = (love.keyboard.isDown("s") and 1 or 0)
                         - (love.keyboard.isDown("w") and 1 or 0)
 
+    --Mouse input
     self.mouse.posX, self.mouse.posY = love.mouse.getPosition()
     self.mouse.lmb = love.mouse.isDown(1)
+
+
+    --Discrete input
+    self.actions.pressed = {}
+    for _, key in ipairs({"space", "escape"}) do
+        local isDown = love.keyboard.isDown(key)
+        if isDown and not self.prev[key] then
+            table.insert(self.actions.pressed, key)
+        end
+        self.prev[key] = isDown
+    end
 end
 
-function Input:get()
+
+function Input:get(key)
+    if key then
+        return self.actions[key] or self.mouse[key]
+    end
+
+    return {
+        actions = {moveX = self.actions.moveX, moveY = self.actions.moveY},
+        mouse = {posX = self.mouse.posX, posY = self.mouse.posY, lmb = self.mouse.lmb}
+    }
 end
 
 return Input
