a439a7aa811e913b1f1111607fb1f446ab159174
Damon <Damon.Connolly@proton.me>
2025-09-26 15:39:19 +0100

    Update render.lua

--- core/render.lua
+++ core/render.lua
@@ -6,16 +6,24 @@ function Render:new(context)
 
     rn.context = context
 
-    rn.queue = {}
-
+    rn.envQueue = {}
+    rn.chrQueue = {}
+    rn.fxQueue = {}
+    rn.uiQueue = {}
     return rn
 end
 
---Consider re-ordering queue so Z doesn't need to be stored and instead infered from list order
-function Render:add(item, z)
+function Render:add(item, queueName)
+    assert(type(item.draw) == "function", "Item must implement draw()")
+    assert(self[queueName], "Invalid queue: " .. tostring(queueName))
+    table.insert(self[queueName], item)
 end
 
 function Render:draw()
+    for _, item in ipairs(self.envQueue) do item:draw() end
+    for _, item in ipairs(self.chrQueue) do item:draw() end
+    for _, item in ipairs(self.fxQueue)  do item:draw() end
+    for _, item in ipairs(self.uiQueue)  do item:draw() end
 end
 
 return Render
